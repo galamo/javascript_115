7004e5862eb4fd703711ef53ffc352ada6c348d5
galamo <galamouyal88@gmail.com>
2025-02-09 18:51:46 +0200

    a

---
+++ programming/9.2/DOM/index.js
@@ -0,0 +1,46 @@
+let tableContent = `  <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td id="markCell">Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>`
+
+function init() {
+
+
+}
+function deleteTable() {
+    const table = document.getElementById("table-data")
+    if (table) {
+        table.innerHTML = ""
+    }
+}
+
+function loadTable() {
+    const table = document.getElementById("table-data")
+    if (table) {
+        table.innerHTML = tableContent
+    }
+}
+init()
