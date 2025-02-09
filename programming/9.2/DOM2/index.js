a969d9fc68c52b193a9a3964328009cf91860ef2
galamo <galamouyal88@gmail.com>
2025-02-09 20:31:48 +0200

    a

--- programming/9.2/DOM2/index.js
+++ programming/9.2/DOM2/index.js
@@ -28,55 +28,65 @@ let tableContent = `  <thead>
 
 
 let more3Rows = `
-                        <tr class="row-table-item">
+                        <tr>
                             <th scope="row">1</th>
                             <td id="markCell">Mark</td>
                             <td>Otto</td>
                             <td>@mdo</td>
                         </tr>
-                        <tr class="row-table-item">
+                        <tr>
                             <th scope="row">2</th>
                             <td>Jacob</td>
                             <td>Thornton</td>
                             <td>@fat</td>
                         </tr>
-                        <tr class="row-table-item">
+                        <tr>
                             <th scope="row">3</th>
                             <td colspan="2">Larry the Bird</td>
                             <td>@twitter</td>
                         </tr>`
 
 
-function init() {
-
-
-}
-function deleteTable() {
-    const table = document.getElementById("table-data")
-    if (table) {
-        table.innerHTML = ""
-    }
+function clearTable() {
+    document.getElementById("table-cars-headers").innerHTML = ""
 }
 
 function loadTable() {
-    const table = document.getElementById("table-data")
-    if (table) {
-        table.innerHTML = tableContent
+    clearTable()
+    const firstElement = carsForRental[0]
+    const fields = Object.keys(firstElement)
+    console.log(firstElement)
+    console.log(fields)
+    const theadTr = document.getElementById("table-cars-headers")
+    if (theadTr) {
+        for (let index = 0; index < fields.length; index++) {
+            const th = document.createElement("th")
+            th.innerText = fields[index].replaceAll("_", " ")
+            theadTr.append(th)
+        }
     }
-}
+    const tBody = document.getElementById("table-cars-body")
+    if (tBody) {
+        for (let index = 0; index < carsForRental.length; index++) {
+            const currentCar = carsForRental[index];
+            const tr = document.createElement("tr")
 
-function addMoreRows() {
+            const tdName = document.createElement("td")
+            tdName.innerText = currentCar.Name
 
-    const tbody = document.getElementById("table-body")
-    if (tbody) {
-        tbody.innerHTML = tbody.innerHTML + more3Rows
-    } else {
-        loadTable()
+
+            const tdMPG = document.createElement("td")
+            tdMPG.innerText = currentCar.Miles_per_Gallon || 0
+
+
+            tr.append(tdName, tdMPG)
+            tBody.append(tr)
+        }
     }
+
+
 }
 
-function changeDom() {
-    const elements = document.getElementsByClassName("row-table-item")
-    elements[0].style.backgroundColor = "yellow"
-}
-init()
+
+
+
