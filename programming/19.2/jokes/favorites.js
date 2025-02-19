b834ca1a5d1a8eb91de1d0470ba2c58df945f6e5
ophiratjb <ophiratjb@gmail.com>
2025-02-19 21:23:49 +0200

    hw

---
+++ programming/19.2/jokes/favorites.js
@@ -0,0 +1,24 @@
+function init() {
+    const favoritesJokesString = localStorage.getItem("favoritesJokes")
+    const favoritesJokesArray = JSON.parse(favoritesJokesString)
+    loadCards(favoritesJokesArray, "jokesContentFavorites", "remove")
+}
+
+init()
+
+function removeFromFavorites(id) {
+    const favoritesJokesString = localStorage.getItem("favoritesJokes")
+    if (favoritesJokesString) {
+        const favoritesJokesArray = JSON.parse(favoritesJokesString)
+        const jokeIndex = getJokeIndexById(id, favoritesJokesArray)
+        if (jokeIndex !== undefined) {
+            favoritesJokesArray.splice(jokeIndex, 1)
+            const favoritesJokesArrayString = JSON.stringify(favoritesJokesArray)
+            localStorage.setItem("favoritesJokes", favoritesJokesArrayString)
+            init()
+        }
+
+    }
+
+
+}
--- programming/19.2/jokes/index.js
+++ programming/19.2/jokes/index.js
@@ -7,28 +7,7 @@ function init() {
 }
 
 
-function loadCards(array, targetContent) {
-    if (!Array.isArray(array)) return; // validate that arrayOfCars is array
-    const content = document.getElementById(targetContent) // Tomer remind me!
-    if (!content) return;
-    content.innerHTML = ""
-    for (let index = 0; index < array.length; index++) {
-        const currentObject = array[index]
-        const cardHtml = getCardTemplate(currentObject)
-        content.innerHTML += cardHtml
-    }
-}
 
-function getCardTemplate(j) {
-    const { id, punchline, type, setup } = j
-    return `<div id="${id}" class="card card-width">
-                <h3>${id}</h3>
-                <h2><span class="badge badge-light" style="background:gray">${type}</span></h2>
-                <h2>${setup}</h2>
-                <h2>${punchline}</h2>
-                <h3> <button class="btn btn-primary" onClick="addToFavorites(${id})"> Add </button> </h3>
-                </div>`
-}
 
 function addToFavorites(id) {
     const jokeToFavorite = getJokeObjById(id, jokes)
@@ -36,10 +15,15 @@ function addToFavorites(id) {
         const favoritesJokesString = localStorage.getItem("favoritesJokes")  // fetch from LS (get)
         if (favoritesJokesString) {
             const favoritesJokesArray = JSON.parse(favoritesJokesString) // JSON.parse 
-            // const found = getJokeObjById(jokeToFavorite.id, favoritesJokesArray)
-            favoritesJokesArray.push(jokeToFavorite) // push into array
-            const favoritesJokesArrayString = JSON.stringify(favoritesJokesArray)// JSON.stringify
-            localStorage.setItem("favoritesJokes", favoritesJokesArrayString)// insert into LS (set)
+            const found = getJokeObjById(jokeToFavorite.id, favoritesJokesArray)
+            if (!found) {
+                favoritesJokesArray.push(jokeToFavorite) // push into array
+                const favoritesJokesArrayString = JSON.stringify(favoritesJokesArray)// JSON.stringify
+                localStorage.setItem("favoritesJokes", favoritesJokesArrayString)// insert into LS (set)
+                alertSuccess()
+            } else {
+                alertError()
+            }
         } else {
             localStorage.setItem("favoritesJokes", JSON.stringify([jokeToFavorite]))
         }
@@ -47,13 +31,18 @@ function addToFavorites(id) {
 
 }
 
-function getJokeObjById(id, jokesArray) {
-    // missing validations 
-    for (let index = 0; index < jokesArray.length; index++) {
-        const current = jokesArray[index];
-        if (current.id === id) {
-            return current;
-        }
-    }
+function alertSuccess() {
+    Swal.fire({
+        title: "Added successfully!",
+        icon: "success"
+    });
 }
+function alertError() {
+    Swal.fire({
+        title: "Already added!",
+        icon: "error"
+    });
+}
+
+
 init()
