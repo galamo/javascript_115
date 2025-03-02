17cff914fb81dc942b6942a18025e6ac5fe2dfcb
shaharsol <shahar@gnift.com>
2025-03-02 19:32:22 +0200

    a

--- programming/2.3/high order functions/find.js
+++ programming/2.3/high order functions/find.js
@@ -24,5 +24,11 @@ if (productIndex > -1) data.products.splice(productIndex, 1)
 console.log("singleObject", singleObject)
 
 
-// array of products ids for all the products with price lower than 50 /map+filter
+// array of products ids for all the products with price lower than 50000 /map+filter
 // array of products return policies for all the products with minimumOrderQuantity > 10  /map+filter
+
+const result1 = data.products.filter(p => p.price < 5000).map(p => p.id)
+console.log(result1)
+
+const result2 = data.products.filter(p => p.minimumOrderQuantity > 10).map(p => p.returnPolicy)
+console.log(result2)
