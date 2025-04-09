8786d0ce8f7dbabe1fa3c5f8b87ffaf59778e3e6
shaharsol <shahar@gnift.com>
2025-04-09 18:40:33 +0300

    a

--- programming/9.4/complexity-currencies/index.js
+++ programming/9.4/complexity-currencies/index.js
@@ -8,3 +8,37 @@ console.log(data)
 
 // Write a function that aggregate region by countries, 
 // {Asia: 12, Europe: 100...}
+
+let result = {}
+data.forEach(c => {
+    if (result[c.region]) {
+        result[c.region] += 1;
+    } else {
+        result[c.region] = 1;
+    }
+});
+console.log(result)
+
+result = {}
+
+
+const res = data.reduce((regionCountry, c) => {
+    if (regionCountry[c.region]) {
+        regionCountry[c.region] += 1
+    } else {
+        regionCountry[c.region] = 1;
+    }
+    return regionCountry;
+}, {})
+console.log(res)
+
+//  Write Reduce function which SUM all the world population
+const popSum = data.reduce(function (sum, c) {
+    if (c.population) {
+        sum = sum + c.population
+    }
+    return sum;
+}, 0)
+console.log(popSum.toLocaleString())
+
+//  Write a function which gathers all the capital cities in array
